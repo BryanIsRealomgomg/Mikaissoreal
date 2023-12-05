b8bf38d05631f21b5fdf270ce57a4788b9e53ca2
BryanIsRealomgomg <132394058+BryanIsRealomgomg@users.noreply.github.com>
2023-12-05 22:27:54 +0800

    Update admir.js

--- admir.js
+++ admir.js
@@ -1,62 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.querySelector('.button');
-    var imageContainer = document.getElementById('imageContainer');
-
-    // Show the button initially with a delay
-    setTimeout(function () {
-        button.classList.add('show');
-    }, 1000);
-
-    // Show the button after a delay
-    button.addEventListener('click', showImage);
-
-    // Make the image clickable
-    imageContainer.addEventListener('click', showImage);
-});
-
 function showImage() {
     var buttonContainer = document.querySelector('.button-container');
-    var button = document.querySelector('.button');
     var imageContainer = document.getElementById('imageContainer');
-    var textElement = document.querySelector('.typing-text');
+    var button = document.querySelector('.button');
 
     buttonContainer.classList.add('active'); // Apply the 'active' class to the button container
 
     // Hide the button after a delay
-    setTimeout(function () {
+    setTimeout(function() {
         button.style.opacity = 0;
-    }, 500);
+    }, 500); // Adjust the delay (in milliseconds) as needed
 
     // Display the image after the button is fully hidden
-    setTimeout(function () {
+    setTimeout(function() {
         button.style.display = 'none';
         imageContainer.style.opacity = 1;
-        textElement.innerHTML = "hey, it's been awhile."; // Replace the existing text
 
-        // Trigger typing animation after image is fully visible
-        textElement.classList.add('typing');
-    }, 1000);
+        // Trigger fade-in effect for the text
+        document.querySelector('.fade-in-text').classList.add('active');
+    }, 1000); // Adjust the delay (in milliseconds) as needed
+
+    // Redirect to the desired website after another delay (adjust as needed)
+    setTimeout(function() {
+        window.location.href = 'https://bryanisrealomgomg.github.io/Mikasaurus/'; // Replace with the desired website URL
+    }, 3000); // Adjust the delay (in milliseconds) as needed
 }
-// Your existing JavaScript functions
 
-// Show the scroll-triggered button after scrolling
-document.addEventListener('scroll', function() {
-    var scrollButtonContainer = document.getElementById('scrollButtonContainer');
-    var scrollButton = document.querySelector('.scroll-button');
-
-    var scrollPosition = window.scrollY + window.innerHeight;
-    var documentHeight = document.documentElement.scrollHeight;
-
-    if (scrollPosition >= documentHeight * 0.8) {
-        // Show the button when 80% of the document is scrolled
-        scrollButtonContainer.classList.add('active');
-    } else {
-        // Hide the button when not scrolled enough
-        scrollButtonContainer.classList.remove('active');
-    }
+// Show the button initially with a delay
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var button = document.querySelector('.button');
+        button.classList.add('show');
+    }, 1000); // Adjust the delay (in milliseconds) as needed
 });
 
-function openNewPage() {
-    // Replace 'your-new-page.html' with the actual path to the new HTML page
-    window.location.href = 'https://bryanisrealomgomg.github.io/Mikasaurus/' ;
-}
+// Show the button after a delay
+document.querySelector('.button').addEventListener('click', showImage);
